ef6d49a625be3cf45d1f3c8db8362a566b1ccded
z6wdc <whitedeathcross@gmail.com>
2024-04-18 04:20:30 +0900

    post

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,22 @@ const express = require('express')
 const Post = require('../model/post')
 const router = express.Router()
 
-router.get('/',async function(req,res,next){
-    const posts = await Post.find()
-    res.send({
+const handleSuccess = function(data) {
+    return {
         'status': true,
-        'data': posts
-    })
+        'data': data
+    }
+}
+
+router.get('/', async function (req, res, next) {
+    const posts = await Post.find()
+    res.send(handleSuccess(posts))
+})
+
+router.post('/', async function (req, res, next) {
+    const b = req.body
+    const newPost = await Post.create(b)
+    res.send(handleSuccess(newPost))
 })
 
 module.exports = router
