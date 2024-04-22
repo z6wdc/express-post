c39c12ce1fa946992989bb9815bc87924ea7ff59
z6wdc <whitedeathcross@gmail.com>
2024-04-22 23:33:56 +0900

    delete

--- routes/posts.js
+++ routes/posts.js
@@ -2,22 +2,70 @@ const express = require('express')
 const Post = require('../model/post')
 const router = express.Router()
 
-const handleSuccess = function(data) {
+const handleSuccess = function (data) {
     return {
         'status': true,
         'data': data
     }
 }
 
+const handleError = function (message) {
+    return {
+        'status': false,
+        'message': message
+    }
+}
+
 router.get('/', async function (req, res, next) {
-    const posts = await Post.find()
-    res.send(handleSuccess(posts))
+    try {
+        const posts = await Post.find()
+        res.send(handleSuccess(posts))
+    } catch (error) {
+        res.send(handleError(error))
+    }
 })
 
 router.post('/', async function (req, res, next) {
     const b = req.body
-    const newPost = await Post.create(b)
-    res.send(handleSuccess(newPost))
+    try {
+        const newPost = await Post.create(b)
+        res.send(handleSuccess(newPost))
+    } catch (error) {
+        res.send(handleError(error))
+    }
+})
+
+router.delete('/', async function (req, res, next) {
+    try {
+        await Post.deleteMany()
+            .then(result => {
+                res.send(handleSuccess(result.deletedCount))
+            })
+            .catch(error => {
+                res.send(handleError(error))
+            })
+    } catch (error) {
+        res.send(handleError(error))
+    }
+})
+
+router.delete('/:id', async function (req, res, next) {
+    const id = req.params.id
+    try {
+        await Post.findByIdAndDelete(id)
+            .then(deleted => {
+                if (deleted != null) {
+                    res.send(handleSuccess(deleted))
+                } else {
+                    res.send(handleError('無此ID'))
+                }
+            })
+            .catch(error => {
+                res.send(handleError(error))
+            })
+    } catch (error) {
+        res.send(handleError(error))
+    }
 })
 
 module.exports = router
